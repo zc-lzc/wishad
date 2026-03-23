9c9d85e1c6fb05736a42d337deb6206f43cfda48
zc-lzc <3148996060@qq.com>
2026-03-23 16:57:30 +0800

    Update pages/order/index_comit.js with new content

--- pages/order/index_comit.js
+++ pages/order/index_comit.js
@@ -8,7 +8,8 @@ Page({
   data: {
     query: {},
     info: {},
-    points: 5
+    points: 5,
+    loading: true
   },
 
   /**
@@ -16,6 +17,9 @@ Page({
    */
   onLoad: function (options) {
     this.setData({ query: options });
+    if (options.sn) {
+      this.getOrderInfo(options.sn);
+    }
   },
 
   /**
@@ -29,42 +33,37 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function (options) {
-    this.getOrderInfo(this.data.query.sn);
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
   },
 
   submit() {
@@ -89,8 +88,28 @@ Page({
   },
 
   getOrderInfo(sn) {
+    if (!sn) {
+      wx.showToast({
+        title: '订单号错误',
+        icon: 'error',
+        duration: 1000
+      });
+      return;
+    }
+    
+    this.setData({ loading: true });
     app.http('/Iorder/getorderinfo', { sn }).then(res => {
-      this.setData({ info: res });
+      this.setData({ 
+        info: res,
+        loading: false
+      });
+    }).catch(err => {
+      this.setData({ loading: false });
+      wx.showToast({
+        title: '加载失败，请重试',
+        icon: 'error',
+        duration: 1000
+      });
     });
   },
 
